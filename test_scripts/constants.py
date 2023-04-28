d68440166a9e0b81075a52246c85ed7e7ade2965
dog3_l0ver <101984812+dog3-l0ver@users.noreply.github.com>
2023-04-28 12:09:46 +0200

    Initial commit

---
+++ test_scripts/constants.py
@@ -0,0 +1,17 @@
+# Temperature coefficient for Stainless Steel 316L (1/°C)
+TEMP_COEFF = 0.00092
+
+# Reference temperature used for measuring the reference resistance (°C)
+REF_TEMP = 20
+
+# Reference resistance measured at the reference temperature (ohms)
+REF_RES = 0.20
+
+# Estimate coil mass (kg)
+COIL_MASS = 0.001136
+
+# Stainless Steel 316L heat capacity (J/g°C)
+HEAT_CAP = 0.5
+
+# Battery maximum current (A)
+MAX_AMPS = 30
